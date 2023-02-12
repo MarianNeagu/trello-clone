272924b56cf9d2b5128bbcda2ad29f05a4ebee0c
Marian Neagu <marianmadalin01@gmail.com>
2023-02-12 16:54:38 +0200

    added cards on home, loading screen, private root home
    Signed-off-by: Marian Neagu <marianmadalin01@gmail.com>

---
+++ src/utils/PrivateRoute.js
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
+const PrivateRoute = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+
+  if (!cookies.user) {
+    // not logged in so redirect to login page with the return url
+    return <Navigate to="/login" />;
+  }
+};
+
+export default PrivateRoute;
